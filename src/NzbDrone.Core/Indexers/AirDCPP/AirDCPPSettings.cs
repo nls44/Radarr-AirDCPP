46eca8c6ddb2df11084c19928ce3f5e0a2627a0f
nls44 <1114597+nls44@users.noreply.github.com>
2025-05-05 17:27:52 +0200

    use basic auth for airdcpp

--- src/NzbDrone.Core/Indexers/AirDCPP/AirDCPPSettings.cs
+++ src/NzbDrone.Core/Indexers/AirDCPP/AirDCPPSettings.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using FluentValidation;
 using NzbDrone.Core.Annotations;
+using NzbDrone.Core.Languages;
 using NzbDrone.Core.Validation;
 
 namespace NzbDrone.Core.Indexers.AirDCPP
@@ -22,7 +24,8 @@ namespace NzbDrone.Core.Indexers.AirDCPP
         public AirDCPPSettings()
         {
             Delay = 3000;
-            MultiLanguages = new List<int>();
+            MultiLanguages = Array.Empty<int>();
+            FailDownloads = Array.Empty<int>();
         }
 
         [FieldDefinition(0, Label = "API base URL")]
@@ -36,7 +39,12 @@ namespace NzbDrone.Core.Indexers.AirDCPP
 
         [FieldDefinition(3, Label = "Delay", HelpText = "Time in milliseconds to wait before retrieving hub search results", Advanced = true)]
         public int Delay { get; set; }
+
+        [FieldDefinition(4, Type = FieldType.Select, SelectOptions = typeof(RealLanguageFieldConverter), Label = "IndexerSettingsMultiLanguageRelease", HelpText = "IndexerSettingsMultiLanguageReleaseHelpText", Advanced = true)]
         public IEnumerable<int> MultiLanguages { get; set; }
+
+        [FieldDefinition(5, Type = FieldType.Select, SelectOptions = typeof(FailDownloads), Label = "IndexerSettingsFailDownloads", HelpText = "IndexerSettingsFailDownloadsHelpText", Advanced = true)]
+        public IEnumerable<int> FailDownloads { get; set; }
 
         public NzbDroneValidationResult Validate()
         {
